5cdb7ab165312db5e5a33d0cdb21afebf0170924
Samuel Yanes <syanes@us.es>
2023-01-20 13:44:52 +0100

    Time stacking wrapped

--- Learning/TrainNoNetwork.py
+++ Learning/TrainNoNetwork.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('.')
+
 from Environment.PatrollingEnvironments import MultiAgentPatrolling
 from Algorithm.RainbowDQL.Agent.DuelingDQNAgent import MultiAgentDuelingDQNAgent
 import numpy as np
@@ -5,30 +8,31 @@ import numpy as np
 N = 4
 
 N = 4
-sc_map = np.genfromtxt('../Environment/example_map.csv', delimiter=',')
+sc_map = np.genfromtxt('./Environment/maps/example_map.csv', delimiter=',')
 visitable_locations = np.vstack(np.where(sc_map != 0)).T
 random_index = np.random.choice(np.arange(0,len(visitable_locations)), N, replace=False)
 initial_positions = np.asarray([[24, 21],[28,24],[27,19],[24,24]])
 
 env = MultiAgentPatrolling(scenario_map=sc_map,
-                           fleet_initial_positions=initial_positions,
-                           distance_budget=200,
-                           number_of_vehicles=N,
-                           seed=0,
-                           detection_length=1,
-                           movement_length=1,
-                           max_collisions=5,
-                           forget_factor=0.5,
-                           attrittion=0.1,
-                           networked_agents=True,
-                           hard_penalization=False,
-                           max_connection_distance=7,
-                           optimal_connection_distance=3,
-                           max_number_of_disconnections=10,
-                           obstacles=False)
+	                           fleet_initial_positions=initial_positions,
+	                           distance_budget=150,
+	                           number_of_vehicles=N,
+	                           seed=10,
+							   miopic=True,
+	                           detection_length=2,
+	                           movement_length=1,
+	                           max_collisions=10,
+	                           forget_factor=0.5,
+	                           attrittion=0.1,
+	                           networked_agents=False,
+							   reward_type='model_changes',
+							   ground_truth_type='shekel',
+	                           obstacles=True,
+                               frame_stacking = 1,
+                               state_index_stacking = (2,3,4))
 
 multiagent = MultiAgentDuelingDQNAgent(env=env,
-                                       memory_size=int(1E6),
+                                       memory_size=int(1E5),
                                        batch_size=64,
                                        target_update=1000,
                                        soft_update=False,
