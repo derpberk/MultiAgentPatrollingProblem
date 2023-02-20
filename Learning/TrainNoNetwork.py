fc539129bc282f724ab5925ea9bede958e357308
Samuel Yanes <syanes@us.es>
2023-02-20 12:03:46 +0100

    Changes

--- Learning/TrainNoNetwork.py
+++ Learning/TrainNoNetwork.py
@@ -5,50 +5,51 @@ from Environment.PatrollingEnvironments import MultiAgentPatrolling
 from Algorithm.RainbowDQL.Agent.DuelingDQNAgent import MultiAgentDuelingDQNAgent
 import numpy as np
 
+
 N = 4
+reward_weights = (0.1, 1)
+
 sc_map = np.genfromtxt('Environment/Maps/example_map.csv', delimiter=',')
-visitable_locations = np.vstack(np.where(sc_map != 0)).T
-random_index = np.random.choice(np.arange(0,len(visitable_locations)), N, replace=False)
 initial_positions = np.asarray([[24, 21],[28,24],[27,19],[24,24]])
 
 env = MultiAgentPatrolling(scenario_map=sc_map,
-							   fleet_initial_positions=initial_positions,
-							   distance_budget=150,
-							   number_of_vehicles=N,
-							   seed=10,
-							   miopic=True,
-							   detection_length=2,
-							   movement_length=2,
-							   max_collisions=10,
-							   forget_factor=0.5,
-							   attrittion=0.1,
-							   networked_agents=False,
-							   reward_type='model_changes',
-							   ground_truth_type='algae_bloom',
-							   obstacles=True,
-							   frame_stacking = 1,
-							   state_index_stacking = (2,3,4),
-							   reward_weights=(0.1, 1))
+							fleet_initial_positions=initial_positions,
+							distance_budget=150,
+							number_of_vehicles=N,
+							seed=10,
+							miopic=True,
+							detection_length=2,
+							movement_length=2,
+							max_collisions=10,
+							forget_factor=0.5,
+							attrittion=0.1,
+							networked_agents=False,
+							reward_type='model_changes',
+							ground_truth_type='algae_bloom',
+							obstacles=True,
+							frame_stacking = 1,
+							state_index_stacking = (2,3,4),
+							reward_weights=reward_weights)
 
 multiagent = MultiAgentDuelingDQNAgent(env=env,
-									   memory_size=int(1E5),
-									   batch_size=64,
-									   target_update=1000,
-									   soft_update=False,
-									   tau=0.0001,
-									   epsilon_values=[1.0, 0.1],
-									   epsilon_interval=[0.0, 0.33],
-									   learning_starts=100,
-									   gamma=0.99,
-									   lr=1e-4,
-									   noisy=False,
-									   train_every=15,
-									   save_every=5000,
-									   distributional=False,
-									   masked_actions=True,
-									   device='cuda:0',
-                   logdir='runs/Experimento_W_1_I_10_dinamico',
-                   eval_episodes=20,
-                   eval_every=2500)
+									memory_size=int(1E5),
+									batch_size=128,
+									target_update=1000,
+									soft_update=True,
+									tau=0.001,
+									epsilon_values=[1.0, 0.05],
+									epsilon_interval=[0.0, 0.33],
+									learning_starts=100,
+									gamma=0.99,
+									lr=1e-4,
+									noisy=False,
+									train_every=15,
+									save_every=5000,
+									distributional=False,
+									masked_actions=True,
+									device='cuda:1',
+				logdir=f'runs/Multiagent/Vehicles_{N}/Experimento_' + reward_weights.replace(', ', '_') + '_short_dinamico',
+				eval_episodes=10,
+				eval_every=1000)
 
-multiagent.train(episodes=50000)
+multiagent.train(episodes=10000)
